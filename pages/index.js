7a542f5130e203229758a07405456ff8ac9f069d
Tobias Bergström <tobiasbergstrom@outlook.com>
2021-12-14 16:56:19 +0100

    asset: Added Arrow
    Added arrow instead of go back in slug

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ export default function Home({ posts }) {
       <main>
         <motion.div
           className="profile"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.75 }}
         >
           <div className="profile--pic">
